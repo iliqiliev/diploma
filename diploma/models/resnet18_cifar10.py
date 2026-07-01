dff5933024cf6f6c52fca0643b09dbcf50ad1b69
Iliya Iliev <iliq0000@proton.me>
2026-07-01 22:52:00 +0300

    ruff: fix resnet18 checks

--- diploma/models/resnet18_cifar10.py
+++ diploma/models/resnet18_cifar10.py
@@ -1,22 +1,28 @@
+"""ResNet18 model for CIFAR10 classification."""
+
 from functools import cache
-from logging import info
 from pathlib import Path
-from typing import cast
+from typing import TYPE_CHECKING, cast
 
 from rich.progress import track
 from torch import Tensor, cat, load, randn_like, save
 from torch.nn import CrossEntropyLoss
 from torch.optim import SGD
 from torch.optim.lr_scheduler import CosineAnnealingLR, LinearLR, SequentialLR
-from torchattacks.attack import Attack
 from torchvision.models import ResNet, resnet18
 
-from ..datasets import CIFAR10_CLASSES, CIFAR10_NORMALIZATION, get_cifar10_loaders
-from ..helpers import BaseModel, DataLoaderTensor, get_torch_device
+from diploma.datasets import CIFAR10_CLASSES, CIFAR10_NORMALIZATION, get_cifar10_loaders
+from diploma.helpers import BaseModel, DataLoaderTensor, get_torch_device, log
+
+if TYPE_CHECKING:
+    from torchattacks.attack import Attack
 
 
 class Cifar10(BaseModel):
-    def __init__(self):
+    """CIFAR-10 model class."""
+
+    def __init__(self) -> None:
+        """Initialize the CIFAR-10 model."""
         super().__init__(
             name="resnet18_cifar10",
             normalization=CIFAR10_NORMALIZATION,
@@ -30,9 +36,7 @@ class Cifar10(BaseModel):
         AT: Attack | None = None,
         GDA: float | None = None,
     ) -> ResNet:
-        """
-        Return the ResNet18 CNN (Convolutional Neural Network)
-        model for CIFAR10 classification.
+        """Return the ResNet18 model for CIFAR10 classification.
 
         Using the default values for `epochs` and `warmup_epochs`
         gives around 83% accuracy.
@@ -43,21 +47,21 @@ class Cifar10(BaseModel):
         The `GDA` (Gaussian Data Augmentation) parameter can be used
         to specify the strength. The value is a `float` type in the range [0, 1].
         """
-
         torch_device = get_torch_device()
 
         if AT is not None:
             attack_name = AT.__class__.__name__
-            info(f"Using AT defence against {attack_name} ...")
+            log.info(f"Using AT defence against {attack_name} ...")
 
             model_path = Path(f"./data/weights/{self.name}_{attack_name}.pth")
             checkpoint_path = Path(f"./data/checkpoints/{self.name}_{attack_name}.pth")
 
         elif GDA is not None:
             if not 0 <= GDA <= 1:
-                raise ValueError("GDA strength must be in the range [0, 1].")
+                error = ValueError("GDA strength must be in the range [0, 1].")
+                raise error
 
-            info(f"Using GDA defence with strength σ={GDA} ...")
+            log.info(f"Using GDA defence with strength σ={GDA} ...")
 
             model_path = Path(f"./data/weights/{self.name}_GDA.pth")
             checkpoint_path = Path(f"./data/checkpoints/{self.name}_GDA.pth")
@@ -73,11 +77,11 @@ class Cifar10(BaseModel):
         model = model.to(torch_device)
 
         if model_path.exists():
-            info(f"Loading model weights from: {model_path}")
+            log.info(f"Loading model weights from: {model_path}")
             _ = model.load_state_dict(load(model_path))
             return model
 
-        info("Model weights not found.")
+        log.info("Model weights not found.")
         train_loader, _ = get_cifar10_loaders()
 
         optimizer = SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=5e-4)
@@ -104,19 +108,20 @@ class Cifar10(BaseModel):
             print(f"Resuming from epoch {start_epoch}/{epochs}: {checkpoint_path}")
 
         for epoch in track(
-            range(start_epoch, epochs), f"Training for {epochs} epochs ..."
+            sequence=range(start_epoch, epochs),
+            description=f"Training for {epochs} epochs ...",
         ):
             _ = model.train()
 
             inputs: Tensor
             labels: Tensor
 
-            for inputs, labels in train_loader:
-                inputs, labels = inputs.to(torch_device), labels.to(torch_device)
+            for _inputs_, _labels_ in train_loader:
+                inputs, labels = _inputs_.to(torch_device), _labels_.to(torch_device)
 
                 if AT is not None:
                     _ = model.eval()
-                    adv_inputs = cast(Tensor, AT(inputs, labels))
+                    adv_inputs = cast("Tensor", AT(inputs, labels))
                     _ = model.train()
 
                     inputs = cat([inputs, adv_inputs], dim=0)
@@ -142,7 +147,7 @@ class Cifar10(BaseModel):
                 checkpoint_path,
             )
 
-        info(f"Saving model to: {model_path}")
+        log.info(f"Saving model to: {model_path}")
         save(model.state_dict(), model_path)
         checkpoint_path.unlink(missing_ok=True)
 
@@ -151,4 +156,5 @@ class Cifar10(BaseModel):
     @property
     @cache
     def loaders(self) -> tuple[DataLoaderTensor, DataLoaderTensor]:
+        """CIFAR-10 train and test data loaders."""
         return get_cifar10_loaders()
